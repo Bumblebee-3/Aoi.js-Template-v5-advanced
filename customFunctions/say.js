22e4d442405a0cd56253dce5da5ceb3be41d5864
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-09-18 12:19:57 +0530

    Create say.js

---
+++ customFunctions/say.js
@@ -0,0 +1,13 @@
+moudle.exports = {
+  name : '$say',
+  params : ['authorid','message'],
+  type : 'aoi.js',
+  code : `**$userTag[{authorid}]** says: **{message}**`
+}
+/*
+Usage: $say[author id;message to say]
+
+Example: $say[$authorID;$message] || $say[818377414367379487;Hello Noobs]
+
+Returns: **Blurr#1447** says: **Hello Noobs**
+*/
