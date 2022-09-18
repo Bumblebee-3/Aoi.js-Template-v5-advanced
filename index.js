38ab14cc2204fab1615a0ae325e4cb992cac9326
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-09-18 19:36:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,33 @@ See ./website/dash.js for more detailed information.
 */
 const website = require("./website/dash.js");
 website.load_website(3000,bot);//3000 is the port you can change it of you want
+
+const fs = require('fs');
+
+function handle_custom_funcs(client,folder) {
+  console.log("\nLoading Custom Functions");
+  console.log("|------------------------------------------|\n\n")
+  
+  let files = fs.readdirSync(`./${folder}`).filter(file => file.endsWith('js'))
+  if (files.length==0){
+    console.log("NO CUSTOM FUNCTION TO LOAD!")
+    console.log("\n\n|------------------------------------------|\n")
+  console.log("Finished Loading Custom Functions\n");
+    return;
+  }
+  files.forEach(x => {
+    bot.functionManager.createCustomFunction({
+      name: require(`./${folder}/${x}`).name,
+      params:require(`./${folder}/${x}`).params,
+      type: require(`./${folder}/${x}`).type,
+      code: require(`./${folder}/${x}`).code
+    })
+    console.log("\nWalking in: "+process.cwd()+`/${folder}/${x}`)
+    console.log("Loaded custom function: "+require(`./${folder}/${x}`).name)
+  });
+  console.log("\n\n|------------------------------------------|\n")
+  console.log("Finished Loading Custom Functions\n");
+
+}
+
+handle_custom_funcs(bot,"customfuncs");//bot is aoi.client and folder here is name of folder for eg:
